24cd178db29386800277ccd7ae283c3f885d2f0f
doravissar <doravissar2@gmail.com>
2024-05-29 00:35:39 +0300

    change readme

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World Dor :) '
+    return 'Hello World , from Dor Avissar (: ) '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
